62aa5256dc6fe0fed714c7b4221a396c2d3e5374
thumberlys10 <108513235+thumbs123@users.noreply.github.com>
2023-10-24 07:05:15 +0700

    entah

--- app/src/main/java/com/example/parkirfirebase/MainActivity.java
+++ app/src/main/java/com/example/parkirfirebase/MainActivity.java
@@ -1,48 +1,68 @@
 package com.example.parkirfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-    Button button;
+
     TextView textView;
     FirebaseUser user;
+
+//    BottomNavigationView ButtonNavigationView;
+//
+//    private BottomNavigationView.OnNavigationItemSelectedListener navigation = new BottomNavigationView.OnNavigationItemSelectedListener(){
+//        @Override
+//        public boolean onNavigationItemSelected(MenuItem item){
+//            Activity a = null;
+//            switch (item.getItemId()){
+//                case R.id.menu_home:
+//                    a = new MainActivity();
+//                    break;
+//                case R.id.menu_parking:
+//                    a = new ParkingActivity();
+//                    break;
+//                case R.id.menu_scan:
+//                    a = new ScanActivity();
+//                    break;
+//                case R.id.menu_history:
+//                    a = new HistoryActivity();
+//                    break;
+//                case R.id.menu_profile:
+//                    a = new ProfileActivity();
+//                    break;
+//            }
+//            return false;
+//        }
+//    };
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
         textView = findViewById(R.id.user_details);
-        user =  auth.getCurrentUser();
-        if (user == null){
-            Intent intent = new Intent(getApplicationContext(),Login.class);
+        user = auth.getCurrentUser();
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
             startActivity(intent);
             finish();
-        }
-        else {
+        } else {
             textView.setText(user.getEmail());
         }
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(),Login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 }
