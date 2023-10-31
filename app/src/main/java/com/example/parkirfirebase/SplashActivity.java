1091c33d187231112909a5c9fe6c0d19c83c1fea
araanv <ameliaarahayu138@gmail.com>
2023-10-31 08:21:25 +0700

    Update SplashActivity.java

--- app/src/main/java/com/example/parkirfirebase/SplashActivity.java
+++ app/src/main/java/com/example/parkirfirebase/SplashActivity.java
@@ -1,25 +1,38 @@
 package com.example.parkirfirebase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends Activity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Thread thread = new Thread(){
-            public void run(){
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Thread thread = new Thread() {
+            public void run() {
                 try {
                     sleep(3000);
-                } catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                }finally {
-                    startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                } finally {
+                    // Cek apakah pengguna sudah login atau belum
+                    FirebaseUser currentUser = mAuth.getCurrentUser();
+                    if (currentUser != null) {
+                        // Pengguna sudah login, pindahkan ke MainActivity
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    } else {
+                        // Pengguna belum login, pindahkan ke halaman login
+                        startActivity(new Intent(SplashActivity.this, Login.class));
+                    }
                     finish();
                 }
             }
