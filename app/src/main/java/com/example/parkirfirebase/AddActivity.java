64ec7476968d13258b9de5de15e40034cc8ee90f
araanv <ameliaarahayu138@gmail.com>
2023-11-03 14:28:59 +0700

    update

--- app/src/main/java/com/example/parkirfirebase/AddActivity.java
+++ app/src/main/java/com/example/parkirfirebase/AddActivity.java
@@ -1,111 +1,132 @@
 package com.example.parkirfirebase;
 
-import androidx.activity.result.ActivityResult;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.StorageReference;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.google.firebase.storage.FirebaseStorage;
-
-import com.google.firebase.storage.UploadTask;
-
-import java.util.Arrays;
-import java.util.Base64;
+import com.google.firebase.storage.StorageReference;
+import java.io.ByteArrayOutputStream;
 
 public class AddActivity extends AppCompatActivity {
 
     private Spinner lokasi;
     private FirebaseStorage firebaseStorage;
     private StorageReference mStorageRef;
-    private FirebaseFirestore firestore;
     private ImageView gambar;
+    private Button cameraBtn;
 
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-        mStorageRef = FirebaseStorage.getInstance().getReference();
 
-        gambar = findViewById(R.id.gambar);
+        firebaseStorage = FirebaseStorage.getInstance();
+        mStorageRef = firebaseStorage.getReference();
+
+        cameraBtn = findViewById(R.id.cameraBtn);
         lokasi = findViewById(R.id.lokasi);
-        String[] values={"Pilih Lokasi","Gedung KHD","Gedung Seroja","Gedung Kuliah Baru","Pos Jembatan","Pos Gedung S"};
-        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(values));
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,arrayList);
+        gambar = findViewById(R.id.gambar); // Pastikan ID ImageView sesuai dengan XML layout
+
+        String[] values = {"Pilih Lokasi", "Gedung KHD", "Gedung Seroja", "Gedung Kuliah Bersama", "Pos Jembatan", "Pos Gedung S"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, values);
         lokasi.setAdapter(arrayAdapter);
 
         lokasi.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (adapterView.getItemAtPosition(i).toString().equals("Pilih Lokasi")) {
-                    String lokasidata = adapterView.getItemAtPosition(i).toString();
-                    Toast.makeText(AddActivity.this, "" + lokasidata, Toast.LENGTH_SHORT).show();
+                String selectedLocation = adapterView.getItemAtPosition(i).toString();
+                if (!selectedLocation.equals("Pilih Lokasi")) {
+                    // Lakukan sesuatu dengan lokasi yang dipilih
+                    Toast.makeText(AddActivity.this, "Lokasi: " + selectedLocation, Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
 
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(AddActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(AddActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+                } else {
+                    startCamera();
+                }
             }
         });
     }
-    public void uploadGambar(View v){
+
+    private void startCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent,101);
+        startActivityForResult(intent, CAMERA_PERMISSION_REQUEST_CODE);
     }
+
     @Override
-    protected void onActivityResult(int requestCode,int resultCode, @Nullable Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if (resultCode == Activity.RESULT_OK){
-            if(requestCode == 101){
-                onCaptureImageResult(data);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Izin kamera diperlukan untuk mengambil gambar.", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void onCaptureImageResult(Intent data) {
-        Bitmap thumbnail = (Bitmap)  data.getExtras().get("data");
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        thumbnail.compress(Bitmap.CompressFormat.JPEG,90,bytes);
-        byte bb[] = bytes.toByteArray();
-        gambar.setImageBitmap(thumbnail);
-
-        uploadToFirebase(bb);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (data != null && data.getExtras() != null) {
+                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                if (thumbnail != null) {
+                    gambar.setImageBitmap(thumbnail);
+                    uploadToFirebase(thumbnail);
+                } else {
+                    Toast.makeText(this, "Gagal mengambil gambar, coba lagi.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
-    private void uploadToFirebase(byte[] bb) {
-        String timestamp = String.valueOf(System.currentTimeMillis());
-        StorageReference sr = mStorageRef.child("images/" + timestamp + ".jpg");
+    private void uploadToFirebase(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, baos);
+        byte[] data = baos.toByteArray();
 
-        sr.putBytes(bb).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                Toast.makeText(AddActivity.this, "Sukses Upload", Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(AddActivity.this, "Gagal upload", Toast.LENGTH_SHORT).show();
-            }
-        });
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        StorageReference imageRef = mStorageRef.child("images/" + timestamp + ".jpg");
+
+        imageRef.putBytes(data)
+                .addOnSuccessListener(taskSnapshot -> {
+                    // Gambar berhasil diunggah, lakukan sesuatu jika diperlukan
+                    Toast.makeText(AddActivity.this, "Sukses Upload", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    // Gagal mengunggah gambar, tampilkan pesan kesalahan
+                    Toast.makeText(AddActivity.this, "Gagal upload: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Log.e("FirebaseStorage", "Gagal upload: " + e.getMessage());
+                });
     }
 }
--- app/src/main/java/com/example/parkirfirebase/MainActivity.java
+++ app/src/main/java/com/example/parkirfirebase/MainActivity.java
@@ -3,22 +3,30 @@ package com.example.parkirfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import android.view.View;
 import com.example.parkirfirebase.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    FloatingActionButton scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Inisialisasi elemen UI setelah mengatur contentView
+        scan = binding.scan;
+
+        // Menampilkan fragment Home saat aplikasi pertama kali dijalankan
         replaceFragment(new HomeFragment());
+
+        // Menghapus latar belakang bottom navigation view
         binding.bottomNavigationView.setBackground(null);
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
@@ -30,17 +38,23 @@ public class MainActivity extends AppCompatActivity {
                 replaceFragment(new HistoryFragment());
             } else if (itemId == R.id.profile) {
                 replaceFragment(new ProfileFragment());
-//            } else if (itemId == R.id.bottomAppBar) {
-//                replaceFragment(new ScanFragment());
             }
             return true;
+        });
+
+        // Menambahkan OnClickListener untuk tombol scan
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScanActivity.class));
+            }
         });
     }
 
     private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
-        fragmentTransaction.commit();
+        // Mengganti fragment pada frame layout
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_layout, fragment)
+                .commit();
     }
 }
