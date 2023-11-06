df54c86372834d0de83f00cf2315c86f1ef32d86
araanv <ameliaarahayu138@gmail.com>
2023-11-06 19:22:08 +0700

    Update AddActivity.java

--- app/src/main/java/com/example/parkirfirebase/AddActivity.java
+++ app/src/main/java/com/example/parkirfirebase/AddActivity.java
@@ -63,20 +63,24 @@ public class AddActivity extends AppCompatActivity {
         progressDialog.setMessage("Please");
 
         pilok = new ArrayList<>();
-        pilok.add("Gedung Kuliah Baru");
+        pilok.add("Gedung Kuliah Bersama");
         pilok.add("Gedung KHD");
         pilok.add("Pos Jembatan");
         pilok.add("Pos Gedung S");
         pilok.add("Gedung Seroja");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, pilok);
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, pilok);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         lokasi.setAdapter(adapter);
-        lokasi.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
         lokasi.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(),adapter.getItem(i),Toast.LENGTH_SHORT).show();
-                Log.e("ID CITY", cities.getDocuments().get(i).getId());
+                if (cities != null && cities.getDocuments().size() > i) {
+                    Log.e("ID CITY", cities.getDocuments().get(i).getId());
+                } else {
+                    Log.e("Error", "Data cities null atau index di luar batas.");
+                }
             }
 
             @Override
@@ -84,9 +88,8 @@ public class AddActivity extends AppCompatActivity {
 
             }
         });
+
         getData();
-
-
 
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,28 +102,34 @@ public class AddActivity extends AppCompatActivity {
             }
         });
     }
-    private void getData(){
+    private void getData() {
         progressDialog.show();
         db.collection("city").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 progressDialog.hide();
-                cities = queryDocumentSnapshots;
-                if (queryDocumentSnapshots.size() > 0) {
-                    pilok.clear();
-                    for (DocumentSnapshot doc : queryDocumentSnapshots) {
-                        pilok.add(doc.getString("name"));
+                if (queryDocumentSnapshots != null) {
+                    Log.d("Firestore", "Data berhasil diambil: " + queryDocumentSnapshots.size() + " dokumen.");
+                    cities = queryDocumentSnapshots;
+                    if (queryDocumentSnapshots.size() > 0) {
+                        pilok.clear();
+                        for (DocumentSnapshot doc : queryDocumentSnapshots) {
+                            pilok.add(doc.getString("name"));
+                        }
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Data tidak tersedia", Toast.LENGTH_SHORT).show();
                     }
-                    adapter.notifyDataSetChanged();
                 } else {
-                    Toast.makeText(getApplicationContext(), "Data tidak tersedia", Toast.LENGTH_SHORT).show();
+                    Log.e("Firestore", "Data kosong atau null.");
                 }
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
                 progressDialog.hide();
-                Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                Log.e("Firestore", "Gagal mengambil data: " + e.getLocalizedMessage());
+                Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
