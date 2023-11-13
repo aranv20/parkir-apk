e5004a9d12cbe342959a2846a8c864b26577e1e5
araanv <ameliaarahayu138@gmail.com>
2023-11-13 14:59:48 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,31 +36,26 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.firebase:firebase-firestore:24.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
+    implementation ("androidx.multidex:multidex:2.0.1")
+    implementation ("com.google.zxing:core:3.4.0")
+    implementation ("me.dm7.barcodescanner:zxing:1.9")
+    implementation ("com.google.firebase:firebase-database:23.0.0")
     implementation("com.google.android.material:material:1.10.0")
     implementation("com.etebarian:meow-bottom-navigation-java:1.2.0")
     implementation ("com.google.mlkit:barcode-scanning:17.2.0")
     implementation ("com.google.android.gms:play-services-mlkit-barcode-scanning:17.1.1")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-    implementation ("com.squareup.picasso:picasso:2.71828")
-
-    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
-
     coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:2.0.4")
-    implementation ("androidx.multidex:multidex:2.0.1")
-
-    implementation ("com.google.zxing:core:3.4.0")
-    implementation ("me.dm7.barcodescanner:zxing:1.9")
-    implementation ("com.google.firebase:firebase-database:23.0.0")
-
 }
