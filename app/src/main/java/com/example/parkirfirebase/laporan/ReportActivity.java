24561e24a258db2b5a8934996ac55218414364fe
ara <ameliaarahayu138@gmail.com>
2023-12-10 20:44:46 +0700

    update report

--- app/src/main/java/com/example/parkirfirebase/laporan/ReportActivity.java
+++ app/src/main/java/com/example/parkirfirebase/laporan/ReportActivity.java
@@ -16,21 +16,26 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ReportActivity extends AppCompatActivity {
 
     private static final String TAG = "ReportActivity";
     private static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
     private FirebaseFirestore firestore;
-    private List<String> receivedReportDataList; // Deklarasikan sebagai anggota kelas
+    private List<String> receivedReportDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,23 +54,28 @@ public class ReportActivity extends AppCompatActivity {
             }
         } else {
             Log.e(TAG, "Error: Tidak ada data laporan yang diterima.");
-            // Tangani kesalahan, mungkin tampilkan toast atau navigasi kembali
             finish();
         }
     }
 
     private void generateReport(List<String> reportDataList) {
-        // Log untuk memeriksa apakah data tiba dengan benar
         Log.d(TAG, "Data laporan yang diterima: " + reportDataList);
 
-        // Lewati pengambilan data dari Firestore karena data sudah ada dalam reportDataList
-
-        // Periksa apakah data tidak kosong sebelum membuat PDF
         if (reportDataList != null && !reportDataList.isEmpty()) {
-            // Periksa dan minta izin WRITE_EXTERNAL_STORAGE jika belum diberikan
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                // Perhatikan bahwa pada baris berikut, saya mengganti nama argumen dari 'reportDataList' menjadi 'receivedReportDataList'
-                createPdf(receivedReportDataList);
+                // Buat salinan dari list untuk menghindari masalah modifikasi selama iterasi
+                List<String> updatedReportDataList = new ArrayList<>(receivedReportDataList);
+
+                // Menghitung jumlah gambar yang perlu diunduh
+                int totalImages = 0;
+                for (String data : updatedReportDataList) {
+                    if (data.toLowerCase().contains("https") && (data.toLowerCase().contains(".jpg") || data.toLowerCase().contains(".png"))) {
+                        totalImages++;
+                    }
+                }
+
+                // Mengeksekusi unduhan gambar dan pembuatan PDF setelah semua gambar diunduh
+                downloadAndProcessImages(updatedReportDataList, totalImages);
             } else {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_STORAGE_PERMISSION);
             }
@@ -74,23 +84,53 @@ public class ReportActivity extends AppCompatActivity {
         }
     }
 
-    private void createPdf(List<String> receivedReportDataList) {
+    private void downloadAndProcessImages(List<String> updatedReportDataList, int totalImages) {
+        AtomicInteger imagesDownloaded = new AtomicInteger(0);
+
+        // Iterasi melalui data laporan
+        for (int i = 0; i < updatedReportDataList.size(); i++) {
+            String data = updatedReportDataList.get(i);
+            if (data.toLowerCase().contains("https") && (data.toLowerCase().contains(".jpg") || data.toLowerCase().contains(".png"))) {
+                final int finalI = i; // Membuat salinan variabel i yang bersifat final
+                // Mengunduh dan memproses gambar
+                downloadImageToLocal(data, new ImageDownloadListener() {
+                    @Override
+                    public void onImageDownloaded(byte[] imageBytes) {
+                        // Menambahkan 1 ke jumlah gambar yang sudah diunduh
+                        int downloadedCount = imagesDownloaded.incrementAndGet();
+
+                        // Jika semua gambar telah diunduh, buat PDF
+                        if (downloadedCount == totalImages) {
+                            createPdf(updatedReportDataList);
+                        }
+                    }
+
+                    @Override
+                    public void onImageDownloadFailed() {
+                        // Handle failure if needed
+                    }
+                });
+            }
+        }
+
+        // Jika tidak ada gambar yang perlu diunduh, langsung buat PDF
+        if (totalImages == 0) {
+            createPdf(updatedReportDataList);
+        }
+    }
+
+    private void createPdf(List<String> updatedReportDataList) {
         try {
-            Log.d(TAG, "Data untuk PDF: " + receivedReportDataList);
+            Log.d(TAG, "Data untuk PDF: " + updatedReportDataList);
 
-            // Tentukan nama file PDF
             String pdfFileName = "laporan_parkir.pdf";
-
-            // Tentukan path untuk menyimpan file PDF
             String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + pdfFileName;
 
-            // Pastikan bahwa direktori untuk menyimpan file PDF ada
             File pdfDirectory = new File(pdfPath);
             if (!pdfDirectory.exists()) {
                 pdfDirectory.mkdirs();
             }
 
-            // Tambahkan nama file ke path
             pdfPath = pdfPath + "/" + pdfFileName;
 
             OutputStream outputStream = new FileOutputStream(pdfPath);
@@ -99,17 +139,12 @@ public class ReportActivity extends AppCompatActivity {
             com.itextpdf.text.pdf.PdfWriter.getInstance(document, outputStream);
             document.open();
 
-            // Tambahkan kepala (header) ke dokumen
             addHeader(document);
 
-            // Implementasi untuk menambahkan data ke PDF
-            for (String data : receivedReportDataList) {
-                // Periksa apakah baris data mengandung URL gambar
+            for (String data : updatedReportDataList) {
                 if (data.toLowerCase().contains("https") && (data.toLowerCase().contains(".jpg") || data.toLowerCase().contains(".png"))) {
-                    // Jika iya, tambahkan gambar ke dokumen
                     addImageFromUrl(document, data);
                 } else {
-                    // Jika tidak, tambahkan teks paragraf
                     document.add(new com.itextpdf.text.Paragraph(data));
                 }
             }
@@ -122,19 +157,14 @@ public class ReportActivity extends AppCompatActivity {
         }
     }
 
-    // Metode untuk menambahkan gambar dari URL ke dokumen PDF
     private void addImageFromUrl(com.itextpdf.text.Document document, String imageUrl) throws DocumentException {
         try {
-            // Menggunakan Image.getInstance untuk menambahkan gambar dari URL ke PDF
-            Image image = Image.getInstance(new URL(imageUrl));
+            // Mengunduh gambar dari URL
+            byte[] imageBytes = downloadImage(imageUrl);
 
-            // Set ukuran gambar sesuai kebutuhan (opsional)
+            // Menyisipkan gambar ke dalam dokumen PDF
+            Image image = Image.getInstance(imageBytes);
             image.scaleToFit(400, 400);
-
-            // Tentukan posisi gambar (opsional)
-            image.setAbsolutePosition(100, 100);
-
-            // Menambahkan gambar ke dokumen
             document.add(image);
         } catch (IOException e) {
             Log.e(TAG, "Error saat menambahkan gambar dari URL: " + e.getMessage());
@@ -142,14 +172,51 @@ public class ReportActivity extends AppCompatActivity {
         }
     }
 
+    private void downloadImageToLocal(String imageUrl, ImageDownloadListener listener) {
+        try {
+            // Mengunduh gambar dari URL
+            byte[] imageBytes = downloadImage(imageUrl);
+
+            // Panggil listener untuk memberi tahu bahwa gambar telah diunduh
+            listener.onImageDownloaded(imageBytes);
+        } catch (Exception e) {
+            Log.e(TAG, "Error saat mengunduh dan menyimpan gambar lokal: " + e.getMessage());
+            e.printStackTrace();
+
+            // Panggil listener untuk memberi tahu bahwa unduhan gagal
+            listener.onImageDownloadFailed();
+        }
+    }
+
+    private byte[] downloadImage(String imageUrl) throws IOException {
+        // Mengunduh gambar dari URL
+        URL url = new URL(imageUrl);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setDoInput(true);
+        httpURLConnection.connect();
+
+        // Membaca data gambar
+        InputStream inputStream = httpURLConnection.getInputStream();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byteArrayOutputStream.write(buffer, 0, bytesRead);
+        }
+
+        // Menutup sumber daya
+        inputStream.close();
+        byteArrayOutputStream.close();
+        httpURLConnection.disconnect();
+
+        return byteArrayOutputStream.toByteArray();
+    }
+
     private void addHeader(com.itextpdf.text.Document document) throws com.itextpdf.text.DocumentException {
-        // Tambahkan elemen kepala (header) ke dokumen di sini
         com.itextpdf.text.Paragraph header = new com.itextpdf.text.Paragraph();
         header.add(new com.itextpdf.text.Chunk("Laporan Parkir Politeknik Negeri Lampung", new com.itextpdf.text.Font(com.itextpdf.text.Font.FontFamily.TIMES_ROMAN, 16, com.itextpdf.text.Font.BOLD)));
         header.add(new com.itextpdf.text.Chunk("\nTanggal: " + getCurrentDate(), new com.itextpdf.text.Font(com.itextpdf.text.Font.FontFamily.TIMES_ROMAN, 12, com.itextpdf.text.Font.NORMAL)));
         header.add(new com.itextpdf.text.Chunk("\n ", new com.itextpdf.text.Font(com.itextpdf.text.Font.FontFamily.TIMES_ROMAN, 12, com.itextpdf.text.Font.NORMAL)));
-
-        // Tambahkan ke dokumen
         document.add(header);
     }
 
@@ -157,26 +224,27 @@ public class ReportActivity extends AppCompatActivity {
         return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
     }
 
-
-    // Metode untuk menangani respons izin
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CODE_STORAGE_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Izin diberikan, buat PDF
                 if (receivedReportDataList != null && !receivedReportDataList.isEmpty()) {
-                    // Perhatikan bahwa pada baris berikut, saya mengganti nama argumen dari 'reportDataList' menjadi 'receivedReportDataList'
                     createPdf(receivedReportDataList);
                 } else {
                     Log.e(TAG, "Error: Data kosong atau null.");
                 }
             } else {
-                // Izin ditolak, tindakan yang sesuai dapat diambil
                 Log.e(TAG, "Izin ditolak. Tidak dapat membuat PDF.");
-                // Tangani penolakan, tampilkan pesan, dll.
                 finish();
             }
         }
+    }
+
+    // Listener untuk menginformasikan ketika gambar telah diunduh
+    interface ImageDownloadListener {
+        void onImageDownloaded(byte[] imageBytes);
+
+        void onImageDownloadFailed();
     }
 }
